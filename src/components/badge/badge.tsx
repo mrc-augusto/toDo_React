2959575dcd3562a3a3dbdce53bc89c68924dd968
Maurício Augusto <mrc_augusto@hotmail.com>
2026-03-25 12:52:05 -0300

    refactor: organize components into subfolders

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from './text';
+import { Text } from '../text/text';
 import {badgeVariants} from './badge-variants'
 import { badgeTextVariants } from './badge-text-variants';
 import type { VariantProps } from 'class-variance-authority';
